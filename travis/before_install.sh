81fcabc246bf6ed360928e87efcda1e48c26b5c4
pinzolo <pinzolo@gmail.com>
2014-06-17 11:24:57 +0900

    Remove unnecessary lines

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -14,13 +14,9 @@ tar xf redmine-${REDMINE_VERSION}.tar.gz
 
 # Copy plugin files to plugin directory
 mkdir redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}
-mv app      redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/app
-mv assets   redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/assets
 mv config   redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/config
-mv db       redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/db
 mv lib      redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/lib
-mv spec     redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/spec
-mv Gemfile  redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/Gemfile
+mv test     redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/test
 mv init.rb  redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/init.rb
 
 # Create necessary files
@@ -29,7 +25,6 @@ test:
   adapter: sqlite3
   database: db/redmine_test.db
 _EOS_
-#cp redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}/spec/fixtures/* redmine-${REDMINE_VERSION}/test/fixtures/
 
 # All move to work directory
 mv redmine-${REDMINE_VERSION}/* .
